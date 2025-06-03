0fa94266b6ee087f9036572de25adf2168da51fc
brendanobilo <brendanxure@outlook.com>
2025-06-03 11:10:38 -0400

    update

--- src/app/api/patient/route.ts
+++ src/app/api/patient/route.ts
@@ -1,2 +1,10 @@
 import {NextRequest, NextResponse} from "next/server";
 import connectDB from "@/config/mongoDBConnection";
+
+export async function GET(req: Request, res: Response) {
+  try {
+    await connectDB();
+  }catch(err){
+
+  }
+}
