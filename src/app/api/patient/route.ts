2ce2f7cb52ce23473b2270ea69447cbd1a3df33a
brendanobilo <brendanxure@outlook.com>
2025-06-06 10:42:03 -0400

    update

--- src/app/api/patient/route.ts
+++ src/app/api/patient/route.ts
@@ -1,10 +1,14 @@
 import {NextRequest, NextResponse} from "next/server";
 import connectDB from "@/config/mongoDBConnection";
+import { getNextSequenceValue } from "@/model/primaryIdCounter";
 
 export async function GET(req: Request, res: Response) {
   try {
     await connectDB();
     const body = await req.json();
+    if(!body._id){
+      body._id = await getNextSequenceValue("Patient", "_id");
+    }
   }catch(err){
     console.log(err);
   }
